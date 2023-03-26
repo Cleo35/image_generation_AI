cb75bb895c5852c5da4a7ffd61bd3ff283d894a7
Celiwe Ngwenya <86663165+Cleo35@users.noreply.github.com>
2023-03-26 16:53:00 +0200

    Update CreatePost.jsx

--- front/src/page/CreatePost.jsx
+++ front/src/page/CreatePost.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
-import { FormField, Loader } from '../components';
+import FormField, Loader from '../components';
 
 const CreatePost = () => {
   const navigate = useNavigate();
