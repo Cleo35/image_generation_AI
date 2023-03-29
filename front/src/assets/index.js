0f88135929bd6a68f25a1790891ebb8a3afed6f6
Cleo35 <86663165+Cleo35@users.noreply.github.com>
2023-03-29 10:18:57 +0200

    front

--- front/src/assets/index.js
+++ front/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
+//import cleo  from './cleo.svg';
 import preview from './preview.png';
 
 export {
   download,
   logo,
+ // cleo,
   preview,
 };
